742c34b56f55cb020f6f3a575c4ac2dc81d340df
Michał Doniec <doniecster@gmail.com>
2021-09-15 14:12:43 +0200

    dodanie funkcji add

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,8 +4,16 @@ let currentResult = defaultResult;
 currentResult = (currentResult +10)*3/2-1;
 
 
-const calcDesc = `(${defaultResult} +10)*3/2-1`;
+const calcDesc = `(${defaultResult} + 10) * 3/2 - 1`;
+const add = (a,b)=>{
+    const result = a+b;
+    return result;
+
+    
+};
+
 
 
 
 outputResult(currentResult, calcDesc);
+addBtn.addEventListener('click', add)
