d790d7dc3fb9ee02c53d6bbbe7879147f3b79be2
Michał Doniec <doniecster@gmail.com>
2021-09-16 10:58:46 +0200

    array dla logentries

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,38 +1,42 @@
 // variables
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 // functions
 
 const getUserNumberInput = () => +userInput.value;
 const createAndWriteLog = (operator, resultBeforeCalc, calcedNumber) => {
-    const calcDescritption = `${resultBeforeCalc} ${operator} ${calcedNumber}`;
-    outputResult(currentResult, calcDescritption);
+  const calcDescritption = `${resultBeforeCalc} ${operator} ${calcedNumber}`;
+   outputResult(currentResult, calcDescritption);
+
+  
 };
 const add = () => {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   createAndWriteLog('+', initialResult, enteredNumber);
-
+  logEntries.push(initialResult);
+  console.log(logEntries);
 };
 const substr = () => {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -= enteredNumber;
   createAndWriteLog('-', initialResult, enteredNumber);
 };
 const multiply = () => {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *= enteredNumber;
   createAndWriteLog('*', initialResult, enteredNumber);
 };
 
 const divide = () => {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /= enteredNumber;
   createAndWriteLog('/', initialResult, enteredNumber);
 };
 
