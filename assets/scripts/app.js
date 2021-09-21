9154a217d7023e7c2daee7b751234a42321dbf62
Michał Doniec <doniecster@gmail.com>
2021-09-21 20:13:01 +0200

    dry calc features

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -37,7 +37,7 @@ const calcResult = (calcType) => {
     caclOperator = '*';
   } else if (calcType === 'DIVIDE') {
     currentResult /= enteredNumber;
-    caclOperator = '/;';
+    caclOperator = '/';
   } else {
     return;
   }
